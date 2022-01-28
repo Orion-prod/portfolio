e3318061afcc45ef95d12f8f12cdfe9dcfee7d9f
Mathis Ubaud <mathisubaud2005@ggmail.com>
2022-01-28 23:05:41 +0100

    add modal img zoom

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ for (var i = 0, len = buttons.length; i < len; i++) {
         header.classList.toggle("active");
     }
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+console.log(img);
+for (var i = 0, len = img.length; i < len; i++) {
+    img[i].onclick = function (){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-img")[0];
+console.log(span)
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
